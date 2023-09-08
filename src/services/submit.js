4a0bbc6d4b241c5b7e8760c764c7b211165b4bcb
rajuEigenMaps <raju@eigenmaps.ai>
2023-09-08 23:26:36 +0530

    fix: submit api

--- src/services/submit.js
+++ src/services/submit.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default async function submit(jobName, itemPageUrl) {
   const response = await axios.post(`http://localhost:3100/api/submit`, {
     jobName,
-    itemPageUrl,
+    itemUrl: itemPageUrl,
   });
   return response.data;
 }
