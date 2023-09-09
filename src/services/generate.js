2542bd59e5abc4b78a22bd5a97dac0bfbca138a7
rajuEigenMaps <raju@eigenmaps.ai>
2023-09-09 13:44:59 +0530

    fix: generate apis

--- src/services/generate.js
+++ src/services/generate.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export async function getItemTitle(jobName, text) {
-  const url = `http://34.100.231.252:8080/generate/item/${jobName}`;
+  const url = `http://34.100.231.252:8080/jobs/generate/item/${jobName}`;
   const response = await axios.post(url, {
     text,
   });
@@ -9,7 +9,7 @@ export async function getItemTitle(jobName, text) {
 }
 
 export async function getItemDescription(jobName, text) {
-  const url = `http://34.100.231.252:8080/generate/description/${jobName}`;
+  const url = `http://34.100.231.252:8080/jobs/generate/description/${jobName}`;
   const response = await axios.post(url, {
     text,
   });
